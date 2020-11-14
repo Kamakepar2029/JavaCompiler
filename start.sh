17f3edbe0cc9ca002ae0278d3c2e8149b10220b5
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-14 13:02:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,12 +6,13 @@ cat /home/runner/work/JavaCompiler/JavaCompiler/app/build/reports/lint-results.h
 cd /home/runner/work/JavaCompiler/JavaCompiler
 ls
 ls app
-git init
 git add *
 git add .gradle
 git add app/build
 git config --global user.email "user@xaker.tk"
 git config --global user.name "USER"
 echo 'No random availible'
+echo "Compiling :-)"
 git commit -m 'resources compiled'
+echo "Pushing!!!"
 git push -u origin master
