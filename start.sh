49569aa4fe8a08b75ef6aa93b8d1074771850976
Kamakepar2029 <58629552+Kamakepar2029@users.noreply.github.com>
2020-11-14 13:09:13 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,11 +8,13 @@ ls
 ls app
 git add *
 git add .gradle
-git add app/build
+git add app/build/outputs/apk/debug/
 git config --global user.email "user@xaker.tk"
 git config --global user.name "USER"
 echo 'No random availible'
 echo "Compiling :-)"
 git commit -m 'resources compiled'
+git branch -M main
+git remote add origin https://github.com/Kamakepar2029/JavaCompiler.git
 echo "Pushing!!!"
-git push -u origin master
+git push -u origin main
