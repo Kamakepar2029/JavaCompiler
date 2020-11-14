41c9a44b15514d8e4d341c595e3da63825c28424
USER <user@xaker.tk>
2020-11-14 12:57:12 +0300

    Unfare

---
+++ start.sh
@@ -0,0 +1,17 @@
+./gradlew build :app:build
+ls /home/runner/work/JavaCompiler/JavaCompiler/app/build/reports/
+ls /home/runner/work/JavaCompiler/JavaCompiler/app/build/
+ls /home/runner/work/JavaCompiler/
+cat /home/runner/work/JavaCompiler/JavaCompiler/app/build/reports/lint-results.html
+cd /home/runner/work/JavaCompiler/JavaCompiler
+ls
+ls app
+git init
+git add *
+git add .gradle
+git add app/build
+git config --global user.email "user@xaker.tk"
+git config --global user.name "USER"
+echo 'No random availible'
+git commit -m 'resources compiled'
+git push -u origin master
